79cd86e353704eee76e1289c94f7a02cd99e1be2
Sachiyo Sugaya <30288392+sachiyology@users.noreply.github.com>
2023-05-12 21:51:16 +0900

    Add files via upload

---
+++ diff.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const resemble = require('node-resemble-js');
+
+//Replace the file names as below
+const file1 = Buffer.from(fs.readFileSync('sc_prod.png'));
+const file2 = Buffer.from(fs.readFileSync('sc_pp.png'));
+
+resemble.outputSettings({ transparency: 0.1 });
+resemble(file1).compareTo(file2).onComplete( data => {
+    if(data.misMatchPercentage >= 0.01) {
+        data.getDiffImage().pack().pipe(fs.createWriteStream('diff_result.png'));
+    }
+});
